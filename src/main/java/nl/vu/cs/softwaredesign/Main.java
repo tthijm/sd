73d543b765a841fd8a8a2d61a556bc2ad2def663
tthijm <59415467+tthijm@users.noreply.github.com>
2024-02-17 15:26:32 +0100

    Format files

--- src/main/java/nl/vu/cs/softwaredesign/Main.java
+++ src/main/java/nl/vu/cs/softwaredesign/Main.java
@@ -1,7 +1,8 @@
 package nl.vu.cs.softwaredesign;
 
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Welcome to Software Design!");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to Software Design!");
+  }
 }

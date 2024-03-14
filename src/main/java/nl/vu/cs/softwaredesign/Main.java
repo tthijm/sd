ab66117791d79698d370adb2feb960f3890dbfa1
amusca <a.musca@student.vu.nl>
2024-03-14 08:41:11 +0100

    Added code for Feature 1

--- src/main/java/nl/vu/cs/softwaredesign/Main.java
+++ src/main/java/nl/vu/cs/softwaredesign/Main.java
@@ -1,8 +1,12 @@
 package nl.vu.cs.softwaredesign;
+import java.util.*;
 
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Welcome to Software Design!");
+
+    //System.out.println("Welcome to Software Design!");
+    Handler start = new Handler();
+    start.loop();
   }
 }

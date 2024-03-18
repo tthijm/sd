9faf2432c5a3d1949b1548013e7f258405eed84b
BMatajsz <90217645+BMatajsz@users.noreply.github.com>
2024-03-18 15:53:36 +0100

    Extract: added helper function

--- src/main/java/archiver/command/Extract.java
+++ src/main/java/archiver/command/Extract.java
@@ -10,14 +10,27 @@ import java.util.zip.ZipInputStream;
 
 public class Extract {
 
+  public File createFile(File dir, ZipEntry ent) throws IOException {
+    File newFile = new File(dir, ent.getName());
+
+    //Check for ZipSlip
+    if (!newFile.getCanonicalPath().startsWith(dir.getCanonicalPath() + File.separator)) {
+      throw new IOException("Error: incorrect path\nEntry: " + ent.getName());
+    }
+    return newFile;
+  }
+
   public void run(String[] args) {
     try {
       String archive = args[0];
       File outputDir = new File(args[1]);
 
+      //Create input stream from the archive
       ZipInputStream inputStream = new ZipInputStream(new FileInputStream(archive));
       ZipEntry entry = inputStream.getNextEntry();
-      while (entry != null) {}
+      while (entry != null) {
+        File newFile = createFile(outputDir, entry);
+      }
     } catch (IOException err) {
       System.out.println("An error has occurred: " + err.getMessage());
     }

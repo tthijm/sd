e24b3bed50981b3608a382c7cacc4af7d6d5a516
BMatajsz <90217645+BMatajsz@users.noreply.github.com>
2024-03-18 15:21:26 +0100

    Extract run progress

--- src/main/java/archiver/command/Extract.java
+++ src/main/java/archiver/command/Extract.java
@@ -4,7 +4,22 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-public class Extract {}
+public class Extract {
+
+  public void run(String[] args) {
+    try {
+      String archive = args[0];
+      File outputDir = new File(args[1]);
+
+      ZipInputStream inputStream = new ZipInputStream(new FileInputStream(archive));
+      ZipEntry entry = inputStream.getNextEntry();
+      while (entry != null) {}
+    } catch (IOException err) {
+      System.out.println("An error has occurred: " + err.getMessage());
+    }
+  }
+}

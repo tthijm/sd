88c0aa6332b979e01d4d8f7000d1d88a37d6edbc
tthijm <59415467+tthijm@users.noreply.github.com>
2024-03-22 17:43:41 +0100

    Add format name and extension

--- src/main/java/archiver/format/Format.java
+++ src/main/java/archiver/format/Format.java
@@ -5,6 +5,33 @@ import java.io.*;
 
 public abstract class Format {
 
+  private final String name;
+  private final String fileExtension;
+  private static final Format[] FORMATS = { new Zip(), new Tar() };
+
+  protected Format(final String name, final String fileExtension) {
+    this.name = name;
+    this.fileExtension = fileExtension;
+  }
+
+  public static Format getInstance(final String x) {
+    for (final Format format : FORMATS) {
+      if (format.getName().equals(x) || x.endsWith(format.getFileExtension())) {
+        return format;
+      }
+    }
+
+    return null;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getFileExtension() {
+    return fileExtension;
+  }
+
   public abstract void compress(File archiveName, File[] fileNames, Config config);
 
   public abstract void decompress(File archiveName, File outputDir);
